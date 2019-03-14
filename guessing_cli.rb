cb51226ffcb76a634a94c14a477ae28945bb7b8d
Jeff M <jkdmarks@gmail.com>
2019-03-14 18:28:34 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,12 @@
 def run_guessing_game
   rand_num = rand(1..6)
+  puts "Guess a number between 1 and 6."
+  guess = gets
   
+  if guess == rand_num
+    puts "You guessed the correct number!"
+  else
+    
 end
 
 run_guessing_game
